dc5d4549290f222b0098545e0ae4362f83768890
Samridh Gupta <samridhgupta@gmail.com>
2021-08-02 16:12:25 +0530

    Remove Js Files

--- tests/Greeting.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Greeting_1 = require("../src/Greeting");
-describe("Greeting", function () {
-    test("should say hello", function () {
-        expect(new Greeting_1.Greeting("Hari").hello()).toEqual("hello Hari");
-    });
-});
