7b802e09797cce475ab94755aa8134eb425d4ad0
Samridh Gupta <samridhgupta@gmail.com>
2021-08-03 18:15:15 +0530

    TEMPERATURE: FInal Implemetions

--- src/Temperature.ts
+++ src/Temperature.ts
@@ -1,27 +1,68 @@
 
 
 // Composition = Square HasA Rectangle = Black-Box reuse
-export class Temperature {
-    private tempInK: number;
-    constructor(temp: string) {
-        const tempNum = parseInt(temp.substring(0, temp.length - 1));
-        if (temp.toLowerCase().includes("c")) {
-            this.tempInK = tempNum + 273.15;
-        } else if (temp.toLowerCase().includes("f")) {
-            this.tempInK = ((tempNum - 32) * 5 / 9) + 273.15;
-        } else {
-            throw new Error();
-        }
+
+
+// type MetricType 
+// export class Temperature {
+//     private tempInK: number;
+//     constructor(temp: string) {
+//         const tempNum = parseInt(temp.substring(0, temp.length - 1));
+//         if (temp.toLowerCase().includes("c")) {
+//             this.tempInK = tempNum + 273.15;
+//         } else if (temp.toLowerCase().includes("f")) {
+//             this.tempInK = ((tempNum - 32) * 5 / 9) + 273.15;
+//         } else {
+//             throw new Error();
+//         }
+//     }
+//     toC = (): string => {
+//         return `${this.tempInK - 273.15}C`;
+//     }
+//     toF = (): string => {
+//         return `${((this.tempInK - 273.15) * 9 / 5) + 32}F`;
+//     }
+//     toK = (): string => {
+//         return `${this.tempInK}K`;
+//     }
+// }
+
+
+// Updated 
+
+
+export class MetricType {
+    name: string; numberOfBaseUnits: number; offset: number;
+    constructor(name: string, numberOfBaseUnits: number, offset: number) {
+        this.name = name;
+        this.numberOfBaseUnits = numberOfBaseUnits;
+        this.offset = offset;
     }
-    toC = (): string => {
-        return `${this.tempInK - 273.15}C`;
+    inBaseUnits(value: number): number {
+        return (value - this.offset) * this.numberOfBaseUnits;
     }
-    toF = (): string => {
-        return `${((this.tempInK - 273.15) * 9 / 5) + 32}F`;
-    }
-    toK = (): string => {
-        return `${this.tempInK}K`;
+    amountForm(valueInBaseUnits: number): number {
+        return valueInBaseUnits / this.numberOfBaseUnits + this.offset
     }
 }
+// Library of 
+export const CELSIUS = new MetricType("C", 1, 0);
+export const FAHRENHEIT = new MetricType("F", 5 / 9, 32);
+export const KELVIN = new MetricType("K", 273.15, 0);
 
-// Composition over Inheritance - Driving Factor
+
+export class TemperatureUnit {
+    value: number;
+    metricType: MetricType;
+    valueInBaseUnits: number;
+
+    constructor(value: number, metricType: MetricType) {
+        this.value = value;
+        this.metricType = metricType;
+        this.valueInBaseUnits = metricType.inBaseUnits(value);
+    }
+
+    in(toUnit: MetricType): TemperatureUnit {
+        return new TemperatureUnit(toUnit.amountForm(this.valueInBaseUnits), toUnit);
+    }
+}
--- tests/Temperature.spec.ts
+++ tests/Temperature.spec.ts
@@ -3,26 +3,26 @@
 // -40C = -40 F
 // 212 F = 100C
 
-import { Temperature } from "../src/Temperature";
+import { CELSIUS, FAHRENHEIT, TemperatureUnit } from "../src/Temperature";
 
-describe("Temperature: Converter", () => {
-    test("Convert 0C to F = 32F", () => {
-        expect(new Temperature("0C").toF()).toEqual("32F");
-    });
-    test("Convert -40C to F = -40F", () => {
-        expect(new Temperature("-40C").toF()).toEqual("-40F");
-    });
-    test("Convert 212F to C = 100C", () => {
-        expect(new Temperature("212F").toC()).toEqual("100C");
-    });
+// describe("Temperature: Converter", () => {
+//     test("Convert 0C to F = 32F", () => {
+//         expect(new Temperature("0C").toF()).toEqual("32F");
+//     });
+//     test("Convert -40C to F = -40F", () => {
+//         expect(new Temperature("-40C").toF()).toEqual("-40F");
+//     });
+//     test("Convert 212F to C = 100C", () => {
+//         expect(new Temperature("212F").toC()).toEqual("100C");
+//     });
 
-    test("Convert 0C to K = 273.15K", () => {
-        expect(new Temperature("0C").toK()).toEqual("273.15K");
-    });
-    // test("Convert Invalid Entry", () => {
-    //     expect(new Temperature("0")).toThrowError();
-    // });
-});
+//     test("Convert 0C to K = 273.15K", () => {
+//         expect(new Temperature("0C").toK()).toEqual("273.15K");
+//     });
+//     // test("Convert Invalid Entry", () => {
+//     //     expect(new Temperature("0")).toThrowError();
+//     // });
+// });
 
 
 // 1. One Class per conversion - CToF, FToC
@@ -35,16 +35,53 @@ describe("Temperature: Converter", () => {
 // Round 2
 // No Extends Keyword
 // Class Names:
-    // Should not end with or / er / tion(Convertor / Manager / Conversion)
-    // Should be a things(noun not verbs)
-    // Comment describing what the class does (later we can get rid of the comment)
+// Should not end with or / er / tion(Convertor / Manager / Conversion)
+// Should be a things(noun not verbs)
+// Comment describing what the class does (later we can get rid of the comment)
 // No combinatorial explosion(Classes or Methods)
 // Avoid indecent exposure(instance variables should be private)
 // Embrace Immutability(instance variables should be final)
 // No If / Else logic(prefer dispatch - table or polymorphism and avoid procedural code)
 // Don’t break Encapsulation (Logic and Data together)
-    // No Getters or Setters
+// No Getters or Setters
 // No Duplication(Literal, Semantic, Structural or Data)
 // Unit Test
 // Be Minimalistic(no surplus code)
 // Clean API(should be simple and intuitive for the caller)
+
+
+
+
+// Breaking down the problem 
+
+/**
+     *
+     * (0°C × 9/5) + 32 = 32°F
+     *               0C = (32F - 32) * 5/9
+     *
+     * 0 C x 9/5 + 32
+     *
+     * 0 Degress Celicius
+     * 0 - value
+     * Unit - (Value, MetricType (name, number of base unit, offset))
+     * C, F
+     * 9 / 5, 5 / 9
+     * 32, -32
+     *
+     * Unit, MetricType (C, F)
+     */
+
+
+
+describe("Temperature:2 Converter", () => {
+    test("Convert 0C to F = 32F", () => {
+        // const celsius = new MetricType("C", 1, 0);
+        // const fahrenheit = new MetricType("F", 5 / 9, 32);
+        // const Kelvin = new MetricType("K", 273.15, 0);
+        expect(new TemperatureUnit(0, CELSIUS).in(FAHRENHEIT)).toEqual(new TemperatureUnit(32, FAHRENHEIT));
+    });
+
+    // test("Convert Invalid Entry", () => {
+    //     expect(new Temperature("0")).toThrowError();
+    // });
+});
